9504fa4e9027793526af8bc4a972b46f79e33670
c4erries <c4erries@gmail.com>
2025-12-10 14:15:23 +0300

    pre beta

---
+++ mlrent/ml.py
@@ -0,0 +1,81 @@
+import numpy as np
+import random
+
+#Квадратичный подход
+def square_trick(weights, bias, data, label, n):
+    if len(data) != len(weights):
+        print('Ащипка')
+        exit()
+    weights = np.array(weights)
+    data = np.array(data)
+    predict = np.sum(weights * data) + bias
+    weights += n*data*(label - predict)
+    bias += n*(label - predict)
+    return weights, bias
+
+#Линейная регрессия
+def liner_regression(data, labels, n = 0.00001, epochs = 100000):
+    bias = 0
+    weights = np.array([1. for _ in range(len(data[0]))])
+    for _ in range(epochs):
+        i = random.randint(0, len(data) - 1)
+        point = data[i]
+        label = labels[i]
+        weights, bias = square_trick(weights, bias, point, label, n)
+        weights[weights < 0] += 0.1 
+        weights[weights > 0] -= 0.1
+        if bias < 0:
+            bias += 0.1
+        elif bias > 0:
+            bias -= 0.1
+    return weights, bias
+
+#Парсер данных
+def parser(file):
+    data = []
+    with open(file, 'r', encoding='utf-8') as f:
+        for string in f:
+            data.append(string.split(','))
+    data = np.array(data[1:])
+    np.random.shuffle(data)
+    labels = data[:, 0]
+    data = data[:, 1:-1]
+    data = data[:, [0, 2, 3, 4, 5, 6, 7, 8, 9]]
+    data[:, 0][data[:, 0] == 'Secondary'] = 0
+    data[:, 0][data[:, 0] == 'New building'] = 1
+    data[:, 2][data[:, 2] == 'Moscow region'] = 0
+    data[:, 2][data[:, 2] == 'Moscow'] = 1
+    data = data.astype(float)
+    labels = labels.astype(float) / 1000
+    test_data = data[:int(len(data)*0.1)]
+    test_lables = labels[:int(len(labels)*0.1)]
+    data = data[int(len(data)*0.1):]
+    labels = labels[int(len(labels)*0.1):]
+    return data, labels, test_data, test_lables
+
+def test(weights, bias, test_l, test_d):
+    weights = np.array(weights)
+    test_d = np.array(test_d)
+    p = 0
+    print('Прогноз, реальные данные, погрешность')
+    for i in range(len(test_d)):
+        predict = np.sum(weights * test_d[i]) + bias
+        print(predict, test_l[i])
+        p += abs(predict - test_l[i])
+    p /= len(test_d)
+    print(p)
+
+
+
+
+
+data, labels, test_data, test_lables = parser('C:/Users/NND01/Desktop/ml/data.csv')
+
+weights, bias = liner_regression(data, labels)
+
+test(weights, bias, test_lables, test_data)
+
+print(weights, bias)
+
+
+
